8df69a5d764d2f2f1fde7a2e48336d0e9c0e7b42
Tim Fleming <tim.j.fleming@gmail.com>
2020-03-13 13:07:21 -0700

    check for None response

--- src/main/resources/blazemeter/Server.py
+++ src/main/resources/blazemeter/Server.py
@@ -16,6 +16,13 @@ params = {
 }
 # make a http request call to the server - access the user api in this test
 response = HttpRequest(params).get('/api/v4/user', contentType='application/json')
+
+# some failures in the org.apache.http.client library can raise silent exceptions.  In that case 'response' will be None.
+if not response:
+    raise Exception(
+        "No response from Blazemeter Server.  Possible SSL negotiation failure."
+    )
+
 # check response status code, if different than 200 exit with error code
 if response.status != 200:
     raise Exception(
